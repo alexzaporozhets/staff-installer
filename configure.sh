5ed8a2388a79dad8c6ca6ea2024e6558373bba6a
Alexey Wasilyev <alexey@wasilyev.ru>
2016-10-21 10:00:38 +0000

    configure script, ssl

---
+++ configure.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+
+ECHO="echo -e"
+
+colors=`tput colors`
+if [ -n "$colors" -a "$colors" -ge 8 ]; then
+  c_red=`tput setaf 1`
+  c_green=`tput setaf 2`
+  c_yellow=`tput setaf 3`
+  c_norm=`tput sgr0`
+  colors=1
+fi
+
+function fail()
+{
+  $ECHO ${c_red}FAIL: ${c_norm}$1
+  GLOBALRET=1
+}
+
+function ok()
+{
+  $ECHO ${c_green}OK: ${c_norm}$1
+}
+
+function warn()
+{
+  $ECHO ${c_yellow}WARN: ${c_norm}$1
+}
+
+$ECHO
+$ECHO = ${c_green} Generating passwords ${c_norm}
+
+S3_ID=identity
+S3_PASS=$(date +%s | sha256sum | base64 | head -c 32)
+GRAFANA_PASS=$(date +%s | sha256sum | base64 | head -c 8)
+
+IP=$(ifconfig | awk '/inet addr/{print substr($2,6)}'|grep -vE '^(127\.0\.0\.1|172\.[12])'|head -n 1)
+
+$ECHO  = ${c_green} Generating config ${c_norm}
+
+cat >.env <<EOT
+AWS_ACCESS_KEY_ID=${S3_ID}
+AWS_SECRET_ACCESS_KEY=${S3_PASS}
+S3PROXY_IDENTITY=${S3_ID}
+S3PROXY_CREDENTIAL=${S3_PASS}
+GF_SECURITY_ADMIN_PASSWORD=${GRAFANA_PASS}
+EOT
+
+$ECHO = ${c_green} Reconfigure containers: ${c_norm}
+
+docker-compose up -d 
+
+$ECHO
+$ECHO = ${c_green} Monitoring: ${c_norm}
+$ECHO
+$ECHO Grafana available as ${c_yellow} "http://${IP}:3003" ${c_norm}
+$ECHO login: ${c_yellow} "admin" ${c_norm} password: ${c_yellow} "${GRAFANA_PASS}" ${c_norm}
