53e78e4001bde23664763d3df4b56d2203de68ac
Alexey Wasilyev <alexey@wasilyev.ru>
2016-11-11 08:20:43 +0000

    remove exited containers and unused images during redeploy

--- redeploy.sh
+++ redeploy.sh
@@ -4,3 +4,5 @@ cd $(dirname $0)
 aws ecr get-login|xargs sudo
 docker-compose pull
 docker-compose up -d
+docker rm $(docker ps -q) >/dev/null 2>&1
+docker rmi $(docker images -f "dangling=true" -q)
