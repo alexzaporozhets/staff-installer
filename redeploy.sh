5690e12e33368d562d29dd239df08eb2a62677fc
Alexey Wasilyev <alexey@wasilyev.ru>
2016-12-05 09:15:20 +0000

    run 2 stats workers by default

--- redeploy.sh
+++ redeploy.sh
@@ -4,5 +4,6 @@ cd $(dirname $0)
 aws ecr get-login|xargs sudo
 docker-compose pull
 docker-compose up -d
+ docker-compose scale api-stats=2
 docker rm $(docker ps -q) >/dev/null 2>&1
 docker rmi $(docker images -q)  >/dev/null 2>&1
