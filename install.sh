1a24fac64737aa30f49f182d1cccd2a3fccfbd73
Alexey Wasilyev <awasilyev@qubell.com>
2017-01-24 14:56:22 +0100

    create admin user during deploy

--- install.sh
+++ install.sh
@@ -10,3 +10,4 @@ fi
 mkdir -p $mount
 grep $device /proc/mounts || mount $device $mount
 docker-compose -p staff up -d
+curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d ' {\n \"name\": \"${STAFF_ADMIN_USERNAME|-admin}\",\n \"email\": \"${STAFF_ADMIN_EMAIL:-admin@staff.com}\",\n \"password\": \"${STAFF_ADMIN_PASSWORD|-changeme}\"\n}' localhost/api/1.0/register/super-admin
